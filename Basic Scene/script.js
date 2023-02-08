5a0d9e8918dbba68df45553ffde357e5b6b1de1d
DiegoCascavita <sportscenter857@gmail.com>
2023-02-08 11:51:16 -0500

    setup of render and canva

--- Basic Scene/script.js
+++ Basic Scene/script.js
@@ -1,6 +1,28 @@
 //scene
 const scene = new THREE.Scene()
+
+
 //Red cube
-const geometry = new THREE.BoxGeometry(1,1,1)//size
-//
-const material = new THREE.MeshBasicMaterial({color: "blue"})|
+//size (1,1,1) equals size
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: "0xff0000"})
+//A mesh object is made up of a geometry and a material. Twelve different geometries are available under "Select Object." 
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+
+//Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+
+// Camera
+//(75 FOV, sizes)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+scene.add(camera)
+
+
+//renderer
+const renderer = new THREE.WebGLRenderer({})
