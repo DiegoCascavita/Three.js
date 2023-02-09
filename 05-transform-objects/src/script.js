5dfe02f5c3e1664a1f8497824a463fb3f195af24
DiegoCascavita <sportscenter857@gmail.com>
2023-02-09 14:23:07 -0500

    using axes helpers and rotate,position and scale

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -12,9 +12,27 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-mesh
 scene.add(mesh)
+//position
+mesh.position.x = 0.7
+mesh.position.y = -0.6
+mesh.position.z = -2
+//or
+mesh.position.set (0.7, -0.6, -2)
+//Scale
+mesh.scale.x = 2
+mesh.scale.y = 1
+mesh.scale.z = 1
+//Rotation
+mesh.rotation.reorder ('XYZ')//AXES ORDER
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = 1
+mesh.rotation.z = 1
 
+
+//AXES helper
+const axesHelper = new THREE.AxesHelper(4)
+scene.add(axesHelper)
 /**
  * Sizes
  */
@@ -28,7 +46,11 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
 scene.add(camera)
+//
+camera.lookAt(mesh.position)
 
 /**
  * Renderer
