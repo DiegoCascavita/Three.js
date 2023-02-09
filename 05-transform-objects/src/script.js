03d08322701e5b588b2777796a4b69bf28fa6ad1
DiegoCascavita <sportscenter857@gmail.com>
2023-02-09 14:35:16 -0500

    practicing

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,16 +9,29 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
+const group = new THREE.Group()
+scene.add(group)
+group.position.set(1, 2, 0)
+group.rotation.set(1,1,2)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: "blue"})
+)
+group.add(cube1)
+
+
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 //position
-mesh.position.x = 0.7
-mesh.position.y = -0.6
-mesh.position.z = -2
+// mesh.position.x = 2
+// mesh.position.y = -0.6
+// mesh.position.z = -2
 //or
-mesh.position.set (0.7, -0.6, -2)
+mesh.position.set (2, -0.6, -2)
 //Scale
 mesh.scale.x = 2
 mesh.scale.y = 1
@@ -50,7 +63,7 @@ camera.position.y = 1
 camera.position.x = 1
 scene.add(camera)
 //
-camera.lookAt(mesh.position)
+// camera.lookAt(mesh.position)
 
 /**
  * Renderer
